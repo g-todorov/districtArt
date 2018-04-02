a472529fee1910627416a346f47964cf1491dbdf
Georgi Todorov <georgi.todorov.19@gmail.com>
2018-04-02 12:36:04 +0300

    Team controller fixes.

--- client/app/scripts/teams/controllers/list.teams.controller.js
+++ client/app/scripts/teams/controllers/list.teams.controller.js
@@ -3,14 +3,14 @@
 angular.module('teams').controller('teamsListController', teamsListController);
 teamsListController.$inject = ['$scope', 'teamsService', '$state', 'API_ENDPOINT', 'AuthService'];
 
-function teamsListController($scope, teamsService, $state, API_ENDPOINT, AuthService) {
+function teamsListController ($scope, teamsService, $state, API_ENDPOINT, AuthService) {
   var currentUserId = AuthService.getUserId();
 
   teamsService.query().$promise.then(function(result) {
-    $scope.teams = result
+    $scope.teams = result;
     angular.forEach($scope.teams, function(value, key) {
-      if (value.visibility == 'private' && value.admins.indexOf(currentUserId) == -1) {
-        value.hideTeam = true
+      if (value.visibility === 'private' && value.admins.indexOf(currentUserId) === -1) {
+        value.hideTeam = true;
       }
     });
   });
